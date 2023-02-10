a0f37df25938320dc91f112b99ec0fd142bb42c9
Mr.li <727607597@qq.com>
2023-02-10 20:47:41 +0800

    upgrade1

--- flower-model/02.代码/streamlit前端.py
+++ flower-model/02.代码/streamlit前端.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6e7db0e6769e760eb40d591597745bb5158aa282fc51586ab79774c3e93cfe1
-size 7851
+oid sha256:12336b8fb41c473398c7f477319076a3cf1560c796ea909ba9652338ad39bdf5
+size 7857
